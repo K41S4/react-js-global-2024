a1f29301daf4ad66792ef51626626bc3e2c5a5e3
K41S4 <kkopeiko@mail.ru>
2024-10-16 22:06:10 +0300

    Part 5: compose all components into one page

--- src/constants.js
+++ src/constants.js
@@ -10,8 +10,88 @@ export const movieData = {
   relevantGenres: ['Crime', 'Comedy'],
 };
 
+export const moviesDummy = [
+  {
+    imageUrl:
+      'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
+    movieName: 'Pulp Fiction',
+    releaseYear: '1994',
+    rating: '8.9',
+    duration: '2h 34min',
+    description:
+      'Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents...',
+    relevantGenres: ['Crime', 'Comedy'],
+  },
+  {
+    imageUrl:
+      'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
+    movieName: 'Pulp Fiction 2',
+    releaseYear: '1994',
+    rating: '8.9',
+    duration: '2h 34min',
+    description:
+      'Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents...',
+    relevantGenres: ['Crime', 'Comedy'],
+  },
+  {
+    imageUrl:
+      'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
+    movieName: 'Pulp Fiction 3',
+    releaseYear: '1994',
+    rating: '8.9',
+    duration: '2h 34min',
+    description:
+      'Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents...',
+    relevantGenres: ['Crime', 'Comedy'],
+  },
+  {
+    imageUrl:
+      'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
+    movieName: 'Pulp Fiction 4',
+    releaseYear: '1994',
+    rating: '8.9',
+    duration: '2h 34min',
+    description:
+      'Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents...',
+    relevantGenres: ['Crime', 'Comedy'],
+  },
+  {
+    imageUrl:
+      'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
+    movieName: 'Pulp Fiction 5',
+    releaseYear: '1994',
+    rating: '8.9',
+    duration: '2h 34min',
+    description:
+      'Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents...',
+    relevantGenres: ['Crime', 'Comedy'],
+  },
+  {
+    imageUrl:
+      'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
+    movieName: 'Pulp Fiction 6',
+    releaseYear: '1994',
+    rating: '8.9',
+    duration: '2h 34min',
+    description:
+      'Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents...',
+    relevantGenres: ['Crime', 'Comedy'],
+  },
+  {
+    imageUrl:
+      'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
+    movieName: 'Pulp Fiction 7',
+    releaseYear: '1994',
+    rating: '8.9',
+    duration: '2h 34min',
+    description:
+      'Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents...',
+    relevantGenres: ['Crime', 'Comedy'],
+  },
+];
+
 export const sortOptions = [
-  { value: 'releaseDate', label: 'Release Date' },
+  { value: 'release_date', label: 'Release Date' },
   { value: 'title', label: 'Title' },
 ];
 
